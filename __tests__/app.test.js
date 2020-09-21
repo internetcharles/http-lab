ce76634553099636d64f04435d6d1e3c56dd8f08
internetcharles <charlesodellsmith@gmail.com>
2020-09-21 14:44:24 -0700

    tests and routes finished

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,33 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('createResponse', () => {
+describe('app routes', () => {
+    it('returns hi html', async () => {
+        const response = await request(app)
+            .get('/');
 
+    expect(response.text)
+        .toEqual(`<html><body><h1>Hi!</h1></body></html>`)
+    });
+    it('returns red html', async () => {
+        const response = await request(app)
+            .get('/red');
+
+    expect(response.text)
+        .toEqual(`<html><body><h1>red</h1></body></html>`)
+    });
+    it('returns green html', async () => {
+        const response = await request(app)
+            .get('/green');
+
+    expect(response.text)
+        .toEqual(`<html><body><h1>green</h1></body></html>`)
+    });
+    it('returns blue html', async () => {
+        const response = await request(app)
+            .get('/blue');
+
+    expect(response.text)
+        .toEqual(`<html><body><h1>blue</h1></body></html>`)
+    });
 });
